6c07cb1a765e8109c5c002caac76a7afc3d73d80
Jonas Santos <90968903+JonasTiago@users.noreply.github.com>
2026-01-08 15:14:57 +0000

    fix lint

--- pages/api/v1/status/index.js
+++ pages/api/v1/status/index.js
@@ -1,5 +1,5 @@
-function status(request, response){
-    response.status(200).send({"chave": "acima da media"});
+function status(request, response) {
+  response.status(200).send({ chave: "acima da media" });
 }
 
 export default status;
