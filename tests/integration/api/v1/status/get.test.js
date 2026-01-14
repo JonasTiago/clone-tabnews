c31b39986a422b84785bfa19a150745c7713c2c7
Jonas Santos <90968903+JonasTiago@users.noreply.github.com>
2026-01-14 15:30:37 +0000

    finish `/api/v1/status` endpoint

--- tests/integration/api/v1/status/get.test.js
+++ tests/integration/api/v1/status/get.test.js
@@ -1,4 +1,18 @@
 test("GET to /api/v1/status should return 200", async () => {
   const response = await fetch("http://localhost:3000/api/v1/status");
   expect(response.status).toBe(200);
+
+  const responseBody = await response.json();
+
+  const parsedUpdateAt = new Date(responseBody.updated_at).toISOString();
+  expect(responseBody.updated_at).toEqual(parsedUpdateAt);
+  expect(responseBody.dependencies.version).toEqual("18.1");
+  expect(responseBody.dependencies.max_connections).toEqual(100);
+  expect(responseBody.dependencies.opened_connections).toEqual(1);
 });
+
+// test.only("Teste GET to /api/v1/status should return 200", async () => {
+//   const response = await fetch(
+//     "http://localhost:3000/api/v1/status?databaseName='; SELECT pg_sleep(4); --",
+//   );
+// });
