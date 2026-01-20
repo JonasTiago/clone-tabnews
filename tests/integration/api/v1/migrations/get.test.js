16be12f117093b10098588b73a57b21c657b091d
Jonas Santos <90968903+JonasTiago@users.noreply.github.com>
2026-01-20 18:40:59 +0000

    fix test

--- tests/integration/api/v1/migrations/get.test.js
+++ tests/integration/api/v1/migrations/get.test.js
@@ -8,9 +8,9 @@ async function cleanDatabase() {
 
 test("GET to /api/v1/migrations should return 200", async () => {
   const response = await fetch("http://localhost:3000/api/v1/migrations");
-  expect(response.status).toBe(200);
 
   const responseBody = await response.json();
+  expect(response.status).toBe(200);
   expect(Array.isArray(responseBody)).toBe(true);
   expect(responseBody.length).toBeGreaterThan(0);
 });
